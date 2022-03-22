16c6e00f80a0611893489281f2ccc12728cb78a5
VitorAGaspar <vitoraugusto.aog@gmail.com>
2022-03-22 19:37:06 -0300

    :sparkles: feat: Add input component

---
+++ src/Components/AddWord/index.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import './style.css';
+
+const AddWord = () => {
+    const [flip, setFlip] = useState(false);
+    const [inputWord, setInputWord] = useState({
+        word: '',
+        definition: '',
+    });
+    const [deck, setDeck] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setDeck((deck) => [...deck, inputWord]);
+        setInputWord({ word: '', definition: '' });
+    };
+
+    return (
+        <div>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input
+                    type="text"
+                    placeholder="Insira uma palavra"
+                    value={inputWord.word}
+                    onChange={(e) =>
+                        setInputWord((prev) => {
+                            return { ...prev, word: e.target.value };
+                        })
+                    }
+                />
+                <input
+                    type="text"
+                    placeholder="Insira a definição"
+                    value={inputWord.definition}
+                    onChange={(e) =>
+                        setInputWord((prev) => {
+                            return {
+                                ...prev,
+                                definition: e.target.value,
+                            };
+                        })
+                    }
+                />
+                <button type="submit">Click</button>
+            </form>
+            <section className="c-deck__section">
+                {deck.map((e, i) => {
+                    return (
+                        <section
+                            onClick={() => setFlip(() => !flip)}
+                            className="c-deck__card"
+                            key={i}
+                        >
+                            {flip ? <p>{e.definition}</p> : <p>{e.word}</p>}
+                        </section>
+                    );
+                })}
+            </section>
+        </div>
+    );
+};
+
+export default AddWord;
