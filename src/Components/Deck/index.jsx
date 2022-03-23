72273d32b9544f972f320c5d5b2360a7e863f084
VitorAGaspar <vitoraugusto.aog@gmail.com>
2022-03-23 18:30:09 -0300

    :hammer: Refactor code to add Redux environment

--- src/Components/Deck/index.jsx
+++ src/Components/Deck/index.jsx
@@ -1,43 +1,15 @@
 import React from 'react';
 import Card from '../Card';
 import './style.css';
+import { useSelector } from 'react-redux';
 
 const Deck = () => {
-    const data = [
-        {
-            word: 'Word',
-            definition: 'Palavra',
-        },
-        {
-            word: 'Faucet',
-            definition: 'Torneira',
-        },
-        {
-            word: 'Green',
-            definition: 'Verde',
-        },
-        {
-            word: 'Bus',
-            definition: 'Ônibus',
-        },
-        {
-            word: 'Cry',
-            definition: 'Chorar',
-        },
-        {
-            word: 'Run',
-            definition: 'Correr',
-        },
-        {
-            word: 'Eat',
-            definition: 'Comer',
-        },
-    ];
+    const deck = useSelector((state) => state);
 
     return (
         <>
             <div className="c-card">
-                {data.map((e, i) => (
+                {deck.map((e, i) => (
                     <Card word={e.word} definition={e.definition} />
                 ))}
             </div>
