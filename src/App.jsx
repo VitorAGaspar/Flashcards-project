bb3e095decce8515f82ac6bb62ec6175d713c1b9
VitorAGaspar <vitoraugusto.aog@gmail.com>
2022-04-22 18:53:05 -0300

    :hammer: refactor: Update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import AddWord from './Components/AddWord';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './global.css';
 import Deck from './Components/Deck';
 import { store } from './Redux/store';
 import { Provider } from 'react-redux';
+import Home from './Pages/Home/Home';
+import ViewDeck from './Pages/ViewDeck/ViewDeck';
+
 const App = () => {
     return (
         <Provider store={store}>
-            <AddWord />
-            <Deck />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/decks/:id" element={<ViewDeck />} />
+                </Routes>
+            </BrowserRouter>
         </Provider>
     );
 };
