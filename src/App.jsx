05f3e811b9b5edbcab2d5b2d602038d434a44f06
VitorAGaspar <vitoraugusto.aog@gmail.com>
2022-05-14 22:16:32 -0300

    :sparkles: feat: Add providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import AddWord from './Components/AddWord';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './global.css';
-import Deck from './Components/Deck';
-import { store } from './Redux/store';
+import Decks from './Components/DeckList';
+import { store, persistor } from './Redux/store';
 import { Provider } from 'react-redux';
 import Home from './Pages/Home/Home';
 import ViewDeck from './Pages/ViewDeck/ViewDeck';
+import Header from './Components/Header';
 
 const App = () => {
     return (
         <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/decks/:id" element={<ViewDeck />} />
-                </Routes>
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/decks/:id" element={<ViewDeck />} />
+                    </Routes>
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     );
 };
